1a3197ebc1690f01d281cbd655fdce075e249491
bruncun <brunocunha@Brunos-iMac.local>
2015-08-18 07:58:06 -0400

    show rails helpers functionality

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,3 +1,8 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+$(document).ready(function() {
+  $('form[data-remote]').on('ajax:success', function(e, data, status, xhr) {
+    $('body').append('<p>Success!</p>');
+  }).on('ajax:error', function(e, error, status, xhr) {
+    // to reproduce error, inspect and change form action to '/article'
+    $('body').append('<p>Error!</p>');
+  });
+});
