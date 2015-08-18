a566f9cdfa4a37d8ca9a10d38bc98f02ab39a111
bruncun <brunocunha@Brunos-iMac.local>
2015-08-18 07:46:07 -0400

    add fancy toggle

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,3 +1,6 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+$(document).ready(function() {
+  $('a[data-font-family]').on('click', function(e) {
+    e.preventDefault();
+    $('body').css('font-family', $(this).data('font-family'));
+  });
+});
